1cdfa8f8a60b655d47583f4d08d19d3ee7f7e5b3
Cali <41587193+cleverguns@users.noreply.github.com>
2021-01-12 11:55:33 +0800

    Add files via upload

---
+++ sysAdTask.py
@@ -0,0 +1,14 @@
+import webbrowser
+# To automate open the google remote
+webbrowser.open('')  
+webbrowser.open('')
+webbrowser.open('')
+webbrowser.open('')
+webbrowser.open('')
+
+
+#open exe files
+import os
+os.startfile('C:\\Program Files (x86)\\Radmin Viewer 3\\Radmin.exe') #sample
+os.startfile() # Remote Desktop for connect to servers
+os.startfile() #open word documents
